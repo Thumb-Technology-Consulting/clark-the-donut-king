24684331c0b684d5aed0f6f75a353f3c360225ad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-03-22 00:56:40 -0400

    refactor(Landing Page): update card titles and descriptions

--- pages/index.js
+++ pages/index.js
@@ -22,31 +22,31 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="pages/recipe.js" className="card">
+            <h3>Donut Hole Ingredients &rarr;</h3>
+            <p>Find out what's inside these delectable dough balls.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Learn How to Make Them &rarr;</h3>
+            <p>Check out the process I use to make these irresistable treats!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>See Testimonials &rarr;</h3>
+            <p>Discover what others are saying about these amazing donut holes.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Order a Dozen! :) &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Purchase some donut holes to support my hobby!
             </p>
           </a>
         </div>
